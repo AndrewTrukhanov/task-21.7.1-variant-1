aec5629f8db78f889e2d85107f09d60e01ae1057
AndrewTrukhanov <andreyreider@gmail.com>
2023-03-13 20:56:30 +0200

    New files are added, file README changed

---
+++ cat_objects.py
@@ -0,0 +1,8 @@
+from cat import Cat
+
+cat1 = Cat("Baron", "male", 2)
+cat2 = Cat("Муся", "девочка", 3)
+
+print("Cat 1: ", "Name - ", cat1.name, ",", "Gender - ", cat1.gender, ",", "Age (years) - ", cat1.age)
+print("\n")
+print("Cat 2: ", "Name - ", cat2.name, ",", "Gender - ", cat2.gender, ",", "Age (years) - ", cat2.age)
